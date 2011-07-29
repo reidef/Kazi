1b306182668cae4c57940158a09e984c412985ea
Reid Gillette <reid.gillette@gmail.com>
2011-07-29 18:03:08 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kazi::Application.config.secret_token = '44b09afff2c99e97d0314083ef376709c9eae607b915a32bb33bcebb3ee930597f7b7ecbab2894842944e7ffea6a723d771f29f59e2dda40c8d5870c0aa6b096'
