646723433395fab611d46a648480c79c31dd44b6
Reid Gillette <reid.gillette@gmail.com>
2011-08-17 22:22:25 -0500

    Added nav menu.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def tag_class_if(tag, text, tag_class, condition=true)
     condition ? content_tag(tag, text, :class => tag_class) : content_tag(tag, text)
   end
+  
+  def navlink(cntrlr_name, link_target, text = nil)
+    text ||= cntrlr_name.humanize
+    a_class = controller.controller_name == cntrlr_name ? "current" : nil
+    link_to text, link_target, :class => a_class
+  end
 end
