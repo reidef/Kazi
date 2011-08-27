0a8973ea4ffca35b2839cb2e8512a297db5387b2
Reid Gillette <reid.gillette@gmail.com>
2011-08-27 18:14:15 -0500

    Added name to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   belongs_to :organization
   
   validates :password, :presence => true, :on => :create
+  validates :password_confirmation, :presence => true, :on => :create
   validates :email, :presence => true,
                     :uniqueness => true,
                     :email_format => true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110827203525) do
+ActiveRecord::Schema.define(:version => 20110827230434) do
 
   create_table "organizations", :force => true do |t|
     t.string   "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20110827203525) do
     t.boolean  "admin",           :default => false
     t.string   "role"
     t.integer  "organization_id"
+    t.string   "name"
   end
 
 end
